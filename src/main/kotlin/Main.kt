d8db51ff84e2aae58967e4960816220ca70e6b6e
Beopsik <mbsik6082@naver.com>
2023-01-12 17:26:24 +0900

    Add when

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,42 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.Exception
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+enum class Color(
+    val r: Int, val g: Int, val b: Int
+){
+    RED(255,0 , 0), ORANGE(255, 165, 0),
+    YELLOW(255, 255, 0), GREEN(0, 255, 0), BLUE(0, 0, 255),
+    INDIGO(75, 0, 130), VIOLET(238, 139, 238);
+
+    fun rgb() = (r * 256 + g) * 256 + b
+}
+
+fun getMnemonic(color: Color) =
+    when (color) {
+        Color.RED -> "Richard"
+        Color.ORANGE -> "Of"
+        Color.YELLOW -> "York"
+        Color.GREEN -> "Gave"
+        Color.BLUE -> "Battle"
+        Color.INDIGO -> "In"
+        Color.VIOLET -> "Vain"
+    }
+
+fun mix(c1: Color, c2: Color) =
+    when (setOf(c1, c2)) {
+        setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
+        setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
+        setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
+        else -> throw Exception("Dirty Color")
+    }
+
+fun mixOptimized(c1: Color, c2: Color) =
+    when {
+        (c1 == Color.RED) and (c2 == Color.YELLOW) || (c1 == Color.YELLOW) and (c2 == Color.RED) -> Color.ORANGE
+        (c1 == Color.YELLOW) and (c2 == Color.BLUE) || (c1 == Color.BLUE) and (c2 == Color.YELLOW) -> Color.GREEN
+        (c1 == Color.BLUE) and (c2 == Color.VIOLET) || (c1 == Color.VIOLET) and (c2 == Color.BLUE) -> Color.INDIGO
+        else -> throw Exception("Dirty Color")
+    }
+fun main() {
+    println(mix(Color.BLUE, Color.YELLOW))
+    println(mixOptimized(Color.BLUE, Color.YELLOW))
 }
