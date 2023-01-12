d90e88b17d8129438f006d18d431e70a960618dc
Beopsik <mbsik6082@naver.com>
2023-01-12 17:55:19 +0900

    Add SmartCast

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,14 @@
-import java.lang.Exception
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
 
-enum class Color(
-    val r: Int, val g: Int, val b: Int
-){
-    RED(255,0 , 0), ORANGE(255, 165, 0),
-    YELLOW(255, 255, 0), GREEN(0, 255, 0), BLUE(0, 0, 255),
-    INDIGO(75, 0, 130), VIOLET(238, 139, 238);
-
-    fun rgb() = (r * 256 + g) * 256 + b
-}
-
-fun getMnemonic(color: Color) =
-    when (color) {
-        Color.RED -> "Richard"
-        Color.ORANGE -> "Of"
-        Color.YELLOW -> "York"
-        Color.GREEN -> "Gave"
-        Color.BLUE -> "Battle"
-        Color.INDIGO -> "In"
-        Color.VIOLET -> "Vain"
-    }
-
-fun mix(c1: Color, c2: Color) =
-    when (setOf(c1, c2)) {
-        setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
-        setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
-        setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
-        else -> throw Exception("Dirty Color")
-    }
-
-fun mixOptimized(c1: Color, c2: Color) =
-    when {
-        (c1 == Color.RED) and (c2 == Color.YELLOW) || (c1 == Color.YELLOW) and (c2 == Color.RED) -> Color.ORANGE
-        (c1 == Color.YELLOW) and (c2 == Color.BLUE) || (c1 == Color.BLUE) and (c2 == Color.YELLOW) -> Color.GREEN
-        (c1 == Color.BLUE) and (c2 == Color.VIOLET) || (c1 == Color.VIOLET) and (c2 == Color.BLUE) -> Color.INDIGO
-        else -> throw Exception("Dirty Color")
+fun eval(e: Expr): Int =
+    when (e) {
+        is Num -> e.value
+        is Sum -> eval(e.left) + eval(e.right)
+        else -> throw IllegalArgumentException("Unknown Expression")
     }
 fun main() {
-    println(mix(Color.BLUE, Color.YELLOW))
-    println(mixOptimized(Color.BLUE, Color.YELLOW))
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+
 }
