898a4e882f664729520f3aa2b31e615d47bde38c
BeobSik <mbsik6082@naver.com>
2023-01-29 13:59:34 +0900

    읽기 전ã용 뷰

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,11 @@
-class User(val id: Int, val name: String, val address: String){
-    override fun toString(): String = "Id: ${this.id} Name: ${this.name} Address: ${this.address}"
-}
-
-fun saveUser(user: User): Boolean {
-    fun validate(
-        value: String,
-        fieldName: String
-    ) {
-        if (value.isEmpty()) {
-            throw IllegalArgumentException("Can't save user ${user.id}: empty $fieldName")
-        }
-    }
-
-    validate(user.name, "Name")
-    validate(user.address, "Address")
-
-    println(user)
-
-    return true
-}
 fun main() {
-    val user=User(213431, "mbs", "Sangdo")
-    println("Save: ${saveUser(user)}")
+    val mutableNums = mutableListOf(3, 1, 4, 1, 5, 9)
+    val toList: List<Int> = mutableNums
+
+    println(toList)
+
+    mutableNums.add(2)
+
+    println(toList)
+    println(mutableNums)
 }
