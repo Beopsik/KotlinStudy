61b26f5e2ffeab89805988a4922f4ed88b081afb
Beopsik <mbsik6082@naver.com>
2023-01-30 14:52:42 +0900

    인터페이스에 선언된 프로퍼티 구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,10 @@
-sealed class Expr {
-    class Num(val value: Int) : Expr()
-    class Sum(val left: Expr, val right: Expr) : Expr()
+interface User {
+    val email: String
+    val nickname: String
+        get() = email.substringBefore("@")
 }
 
-fun eval(e: Expr): Int =
-    when (e) {
-        is Expr.Num -> e.value
-        is Expr.Sum -> eval(e.left) + eval(e.right)
-    }
-
 fun main() {
-    println(eval(Expr.Sum(Expr.Num(1), Expr.Num(2))))
+    println(PrivateUser("test@kotlinlang.org").nickname)
+    println(SubscribeUser("test@kotlinlang.org").nickname)
 }
