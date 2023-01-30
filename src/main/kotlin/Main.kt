57e70683d5edac6101c56cd1b6104d9ac40d9862
Beopsik <mbsik6082@naver.com>
2023-01-30 16:28:53 +0900

    모든 클래스가 정의해야 하는 메서드

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,20 @@
-class LengthCounter {
-    var counter: Int = 0
-        private set
-
-    fun addWord(word: String) {
-        counter += word.length
+class Client(val name: String, val postalCode: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (other == null || other !is Client) {
+            return false
+        }
+        return name == other.name && postalCode == other.postalCode
     }
+
+    override fun toString(): String = "Client (name=$name, postalCode=$postalCode)"
+
+    override fun hashCode(): Int = name.hashCode() * 31 + postalCode
 }
 
 fun main() {
-    val lc = LengthCounter()
-    lc.addWord("word")
-    println(lc.counter)
+    val client1 = Client("mbs", 1)
+    val client2 = Client("mbs", 1)
+
+    println(client1)
+    println(client1 == client2)
 }
