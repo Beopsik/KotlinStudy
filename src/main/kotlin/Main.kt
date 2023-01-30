3ae0e043aa3f0a23c52a33a2fb51e6c0aa98faec
Beopsik <mbsik6082@naver.com>
2023-01-30 13:55:04 +0900

    내부 클래스와 중첩된 클래스

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,15 @@
-abstract class Animated {
-    abstract fun animate()
+import java.io.Serializable
 
-    open fun stopAnimating() {
-    }
-
-    fun animatedTwice() {
+interface State: Serializable
+interface View {
+    fun getCurrentState(): State
+    fun restoreState(state: State) {}
+}
+class Button : View {
+    override fun getCurrentState(): State = ButtonState()
+    override fun restoreState(state: State) {}
+    inner class ButtonState: State{
+        fun getOuterReference(): Button = this@Button
     }
 }
 fun main() {
