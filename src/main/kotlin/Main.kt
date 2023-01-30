dbc382d47aad9c532e5b103cf48b3b0503616a7a
Beopsik <mbsik6082@naver.com>
2023-01-30 13:21:20 +0900

    open

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,17 @@
-data class Num(val num1: Int, val num2: Int, val num3: Int, val num4: Int, val num5: Int, val num6: Int)
-fun main() {
-    val list = listOf("a", "b", "c", "d", "e", "f", "g", "h")
-    val (a, b, c, d, e) = list
-    println("$a, $b, $c, $d, $e")
+interface Clickable {
+    fun click()
+    fun showOff() = println("I'm clickable")
+}
 
-    val numData = Num(1, 2, 3, 4, 5, 6)
-    val (n1, n2, n3, n4, n5, n6) = numData
-    println("$n1, $n2, $n3, $n4, $n5, $n6")
+open class RichButton : Clickable {
+    fun disable() {}
+
+    open fun animate() {}
+
+    final override fun click() {
+        println("Rich Button")
+    }
+}
+fun main() {
+
 }
