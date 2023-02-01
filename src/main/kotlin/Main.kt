72a0bdec579bac75e38dbada3bd5b21847a06148
Beopsik <mbsik6082@naver.com>
2023-02-01 16:46:33 +0900

    시퀀스 만들기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
-fun main() {
-    val naturalNumbers = generateSequence(0) { it + 1 }
-    val numbersTo100 = naturalNumbers.takeWhile { it <= 100 }
-    println(numbersTo100.sum())
+import java.io.File
 
+fun main() {
+    fun File.isInsideHiddenDirectory() = generateSequence(this) { it.parentFile }.any { it.isHidden }
+    val file = File("/Users/svtk/.HiddenDir/a.txt")
+
+    println(file.isInsideHiddenDirectory())
 }
