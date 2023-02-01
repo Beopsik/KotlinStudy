951228e99776caff96174a4388d69bfb86e7d01e
Beopsik <mbsik6082@naver.com>
2023-02-01 16:10:12 +0900

    groupBy

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,6 @@
 data class Person(val name: String, val age: Int)
 fun main() {
-    val people= listOf(Person("Alice", 27), Person("Bob", 31))
-    val canBeInClub27={p:Person-> p.age <= 27}
+    val people= listOf(Person("Alice", 27), Person("Bob", 31), Person("Carol", 27))
 
-    println(people.all(canBeInClub27))
-    println(people.any(canBeInClub27))
-    println(people.count(canBeInClub27))
-    println(people.find(canBeInClub27))
+    println(people.groupBy{it.age })
 }
