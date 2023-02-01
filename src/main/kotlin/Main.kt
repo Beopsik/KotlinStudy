1225b38716f25e44c6cad51445f1a6b2689fbe53
Beopsik <mbsik6082@naver.com>
2023-02-01 15:08:55 +0900

    람다 안에서 바깥 함수의 로컬 변수 변경

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,18 @@
-fun printMessageWithPrefix(messages: Collection<String>, prefix: String) {
-    messages.forEach{
-        println("$prefix $it")
+fun printProblemCounts(responses: Collection<String>) {
+    var clientErrors = 0
+    var serverErrors = 0
+    responses.forEach {
+        if (it.startsWith("4")) {
+            clientErrors++
+        }else if (it.startsWith("5")) {
+            serverErrors++
+        } else {
+            // Http Status code 200
+        }
     }
+    println("$clientErrors client errors, $serverErrors server errrors")
 }
 fun main() {
-    val errors= listOf("403 Forbidden", "404 Not Found")
-    printMessageWithPrefix(errors, "Error:")
+    val responses = listOf("200 OK", "418 I'm a teapot", "500 Internal Server Error")
+    printProblemCounts(responses)
 }
