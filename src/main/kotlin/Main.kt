e6b2dbc4de0cafb5ff606231fc4ead08eef9ed5d
Beopsik <mbsik6082@naver.com>
2023-02-01 15:33:58 +0900

    멤버 참조

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,6 @@
-fun printProblemCounts(responses: Collection<String>) {
-    var clientErrors = 0
-    var serverErrors = 0
-    responses.forEach {
-        if (it.startsWith("4")) {
-            clientErrors++
-        }else if (it.startsWith("5")) {
-            serverErrors++
-        } else {
-            // Http Status code 200
-        }
-    }
-    println("$clientErrors client errors, $serverErrors server errrors")
-}
+data class Person(val name: String, val age: Int)
 fun main() {
-    val responses = listOf("200 OK", "418 I'm a teapot", "500 Internal Server Error")
-    printProblemCounts(responses)
+    val createPerson = ::Person
+    val p=createPerson("Alice", 29)
+    println(p)
 }
