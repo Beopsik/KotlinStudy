832e8e2db81dbc395f25072345914f4865cd7d3d
Beopsik <mbsik6082@naver.com>
2023-02-01 15:56:45 +0900

    filter, map

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 data class Person(val name: String, val age: Int)
 fun main() {
-    val createPerson = ::Person
-    val p=createPerson("Alice", 29)
-    println(p)
+    val people= listOf(Person("Alice", 29), Person("Bob", 31))
+    println(people.filter { it.age >= 30 })
+    println(people.map { it.name })
+
+    println(people.filter { it.age>=30 }.map { it.name })
+
+    val maxAge=people.maxBy { it.age }!!.age
+    println(people.filter { it.age == maxAge })
 }
