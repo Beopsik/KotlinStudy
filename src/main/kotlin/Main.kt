2a2b27feb617f20d9fb2ac6cf4dffb8e32e5b5a1
Beopsik <mbsik6082@naver.com>
2023-02-01 14:54:12 +0900

    디폴트 파라미터 이름 it 사용

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,7 @@ data class Person(val name: String, val age: Int)
 fun main() {
     val people = listOf(Person("Alice", 29), Person("Bob", 31))
     println(people.maxBy { it.age })
+
+    val names=people.joinToString(" ") { it.name }
+    println(names)
 }
