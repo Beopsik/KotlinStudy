c7678e9f7555acaca7bc384e4622be999b1a1a61
Beopsik <mbsik6082@naver.com>
2023-02-02 13:12:14 +0900

    with 함수 리팩토링

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,13 @@
 import java.lang.StringBuilder
 
-fun alphabet(): String {
-    val stringBuilder = StringBuilder()
-    return with(stringBuilder) {
-        for (letter in 'A'..'Z') {
-            this.append(letter)
-
-        }
-        append("\nNow I know the alphabet!")
-        this.toString()
+fun alphabet(): String = with(StringBuilder()) {
+    for (letter in 'A'..'Z') {
+        append(letter)
     }
+    append("\nNow I know the alphabet!")
+    toString()
 }
+
 fun main() {
     println(alphabet())
 }
