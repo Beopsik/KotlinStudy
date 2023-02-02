480cfb4fd80e1e2b11d0995d40584deb79a861b6
Beopsik <mbsik6082@naver.com>
2023-02-02 14:05:35 +0900

    엘비스 연산자

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,7 @@ class Address(val streetAddress: String, val zipCode: Int, val city: String, val
 class Company(val name: String, val address: Address?)
 class Person(val name: String, val company: Company?)
 
-fun Person.countryName(): String {
-    val country=this.company?.address?.country
-
-    return country ?: "Unknown"
-}
+fun Person.countryName(): String = company?.address?.country ?: "Unknown"
 
 fun main() {
     val person = Person("Dmitry", null)
