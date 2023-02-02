85645cbacf99e146d7f54089375bf5315f9ae240
Beopsik <mbsik6082@naver.com>
2023-02-02 13:55:34 +0900

    안전한 호출 연쇄

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
-class Employee(val name: String, val manager: Employee?)
-fun managerName(employee: Employee): String? = employee.manager?.name
+class Address(val streetAddress: String, val zipCode: Int, val city: String, val country: String)
+class Company(val name: String, val address: Address?)
+class Person(val name: String, val company: Company?)
+
+fun Person.countryName(): String {
+    val country=this.company?.address?.country
+
+    return country ?: "Unknown"
+}
 
 fun main() {
-    val ceo = Employee("Da Boss", null)
-    val developer = Employee("Bob Smith", ceo)
-    println(managerName(developer))
-    println(managerName(ceo))
+    val person = Person("Dmitry", null)
+    println(person.countryName())
 }
