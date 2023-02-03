f86e350ff514135ef2d41e874735c64cd00b3608
Beopsik <mbsik6082@naver.com>
2023-02-03 11:11:02 +0900

    자바 인터페이스를 여러 다른 널 가능성으로 구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,15 @@
-fun yellAt(person: Person) {
-    println((person.name ?: "Anyone").toUpperCase() + "!!!")
+class StringPrinter : StringProcessor {
+    override fun process(value: String) {
+        println(value)
+    }
+}
+
+class NullableStringPrinter : StringProcessor {
+    override fun process(value: String?) {
+        if (value != null) {
+            println(value)
+        }
+    }
 }
 fun main() {
-    yellAt(Person(null))
 }
