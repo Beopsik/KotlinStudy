243a5cc7708d7d23d5e321d971cb7ce383b4a78c
Beopsik <mbsik6082@naver.com>
2023-02-06 10:20:45 +0900

    7.4 리팩토링 전

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,5 @@
-fun processString(str: String?) =
-    str?.let {
-        when {
-            it.isEmpty() -> "Empty"
-            it.isBlank() -> "Blank"
-            else -> it.toUpperCase()
-        }
-    } ?: "Null"
 fun main() {
-    println(processString("hello"))
+    val numbers = mutableListOf("one", "two", "three", "four", "five")
+    val resultList=numbers.map { it.length }.filter { it > 3 }
+    println(resultList)
 }
